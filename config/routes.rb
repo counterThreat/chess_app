a8724458ea86c61c1b437c154346ff4f4d1b8748
Kathleen French <kfrench09@gmail.com>
2017-03-30 14:25:42 -0400

    fixed edit registration page compatible with username function

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
