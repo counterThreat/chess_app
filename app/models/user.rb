af5f103c4b3a093af12da7c01353b2c63d9edd73
Kathleen French <kfrench09@gmail.com>
2017-04-05 16:25:53 -0400

    rubcop fixes on my files

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,42 @@ class User < ApplicationRecord
          :trackable,
          :validatable,
          :omniauthable,
-         :authentication_keys => [:login],
-         :omniauth_providers => [:google_oauth2]
+         authentication_keys: [:login],
+         omniauth_providers: [:google_oauth2]
 
   validates :username,
-    :presence => true,
-    :uniqueness => {
-      :case_sensitive => false
-    }
+            presence: true,
+            uniqueness: {
+              case_sensitive: false
+            }
 
   validate :validate_username
   attr_accessor :login
   has_and_belongs_to_many :oauth_providers
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      data = session['devise.oogle_oauth2_data']
+      if data && session['devise.oogle_oauth2_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+      password = Devise.friendly_token[0, 20]
+      user = User.create( #name: data["name"],
+        email: data['email'], username: data['email'],
+        password: password, password_confirmation: password
+      )
+
+    end
+    user
+  end
 
   private
 
@@ -33,32 +57,6 @@ class User < ApplicationRecord
   end
 
   def validate_username
-    if User.where(email: username).exists?
-      errors.add(:username, :invalid)
-    end
+    errors.add(:username, :invalid) if User.where(email: username).exists?
   end
-
-  def self.new_with_session(params, session)
-  super.tap do |user|
-    if data = session["devise.oogle_oauth2_data"] && session["devise.oogle_oauth2_data"]["extra"]["raw_info"]
-      user.email = data["email"] if user.email.blank?
-    end
-  end
-  end
-
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(:email => data["email"]).first
-          # Uncomment the section below if you want users to be created if they don't exist
-          unless user
-            password = Devise.friendly_token[0,20]
-               user = User.create(#name: data["name"],
-                  email: data["email"], username: data["email"],
-                  password: password, password_confirmation: password)
-
-          end
-    user
-  end
-
-
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -52,8 +52,8 @@ Devise.setup do |config|
   # These keys will have whitespace before and after removed upon creating or
   # modifying a user and when used to authenticate or find a user. Default is :email.
   config.strip_whitespace_keys = [:username]
-  config.reset_password_keys = [ :username ]
-  config.confirmation_keys = [ :username ]
+  config.reset_password_keys = [:username]
+  config.confirmation_keys = [:username]
 
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
@@ -254,10 +254,7 @@ Devise.setup do |config|
   # up on your models and hooks.
 
   require 'omniauth-google-oauth2'
-  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-    :scope => "email, profile"
-  }
-
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], scope: 'email, profile'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
