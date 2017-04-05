c87f0aafbdaf67f26233ab1a77ecdcf7928c7c9b
Kathleen French <kfrench09@gmail.com>
2017-04-05 16:31:03 -0400

    routes rubocop fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root 'static_pages#index'
 
   resources :users
-
 end
