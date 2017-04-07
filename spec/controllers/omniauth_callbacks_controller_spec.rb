d44b57dd9b4eed1d82cb4186c160077b0aeb0112
Kathleen French <kfrench09@gmail.com>
2017-04-06 21:11:41 -0400

    fixed rubocop errors

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe OmniauthCallbacksController, type: :controller do
+  describe "omniauthcallbacks google_auth2 action" do
+    it "successfully authenticates the user" do
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "users controller actions" do
+    it "successfully" do
+    end
+  end
 end
