d7a5ac7292c17c15715ae85eb7d2bccffe48997c
Kathleen French <kfrench09@gmail.com>
2017-04-06 21:23:45 -0400

    passing all rubocop errors

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   describe "static_pages#index action" do
-    it "successfully shows the page" do
+    it "succesfully shows the page" do
       get :index
       expect(response).to have_http_status(:success)
     end
