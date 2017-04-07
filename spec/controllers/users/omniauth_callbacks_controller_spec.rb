76a5b59311589ec90f57d46dc56ee8fd84354019
Kathleen French <kfrench09@gmail.com>
2017-04-06 23:00:57 -0400

    l

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe OmniauthCallbacksController, type: :controller do
-  describe "omniauthcallbacks google_auth2 action" do
-    it "successfully authenticates the user" do
-    end
-  end
-end
