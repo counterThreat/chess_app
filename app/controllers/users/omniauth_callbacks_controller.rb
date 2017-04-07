02f3de919c6d0ca1c145a13baa573cc59c919e71
Kathleen French <kfrench09@gmail.com>
2017-04-06 22:25:34 -0400

    fixed migration issue

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,13 @@
-module Users
-  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  @user = User.from_omniauth(request.env['omniauth.auth'])
 
-    def google_oauth2
-      if @user.persisted?
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-        sign_in_and_redirect @user, event: :authentication
-      else
-        session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-      end
+  def google_oauth2
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
 end
