07a5a7915bb7fbf62f3b57ba8ce63a7a77864a62
Kathleen French <kfrench09@gmail.com>
2017-04-09 17:11:47 -0400

    set up basic game model and game controller

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,12 @@ class Game < ApplicationRecord
   has_many :pieces
   has_many :white_player, class_name: 'User', foreign_key: 'white_player_id'
   has_many :black_player, class_name: 'User', foreign_key: 'black_player_id'
+
+  validates :name, presence: true
+
+  def associate_pieces!(user, color)
+    pieces.where(color: color).each do |piece|
+      user.piece << piece
+    end
+  end
 end
