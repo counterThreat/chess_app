19cde502ed9e02cc51d7995b1aad61c5ce462275
DavidMichaelPena <alexamenos@gmail.com>
2017-04-13 12:17:51 +0000

    fixing gems

--- db/migrate/20170412054234_create_pawns.rb
+++
@@ -1,8 +0,0 @@
-class CreatePawns < ActiveRecord::Migration[5.0]
-  def change
-    create_table :pawns do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20170412054346_create_kings.rb
+++
@@ -1,8 +0,0 @@
-class CreateKings < ActiveRecord::Migration[5.0]
-  def change
-    create_table :kings do |t|
-
-      t.timestamps
-    end
-  end
-end
