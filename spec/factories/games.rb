23e44366c1b058a52e01b0229e525a3afdb807fe
DavidMichaelPena <alexamenos@gmail.com>
2017-04-13 13:31:51 +0000

    Attempting to fix factories game file(s)

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
+
   factory :game do
     name 'test game'
+    association :game, :white_user, factory: :user
+    x_position 0
+    y_position 0
+    color false
   end
 
   factory :game_with_white_player, class: Game do
@@ -17,6 +22,23 @@ FactoryGirl.define do
     name 'test game'
     white_player_id 0
     black_player_id 1
+  end
+
+  factory :game_white_player, class: Game do
+    name 'factory girl test'
+    white_player_id 1
+  end
+
+  factory :king, parent: :piece do
+    piece_type "knight"
+    x_position 2
+    y_position 1
+  end
+
+  factory :bishop, parent: :piece do
+    piece_type "rook"
+    x_position 1
+    y_position 1
   end
 end
 
