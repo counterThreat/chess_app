93cfa9a3499f173b16bc8ce38c94aca83f01fe97
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-04-14 19:36:22 +0000

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414001844) do
+ActiveRecord::Schema.define(version: 20170414193439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(version: 20170414001844) do
     t.integer  "game_id"
     t.integer  "user_id"
     t.boolean  "moved"
-    t.boolean  "captured"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "captured",   default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
