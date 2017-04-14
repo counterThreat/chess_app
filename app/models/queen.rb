daf839eee38da8bb9fa880c091576af485e43406
DavidMichaelPena <alexamenos@gmail.com>
2017-04-14 18:27:25 +0000

    queen_model improved!

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,8 @@
 class Queen < Piece
+  def valid_move?(x, y)
+    return false unless super(x, y)
+    return false if is_obstructed?(x, y)
+    diagonal_move?(x, y) || horizontal_move?(x, y) || vertical_move?(x, y)
+  end
 end
+
