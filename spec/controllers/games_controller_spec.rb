e90ad0823187173a33993bbef237530be6b40f7f
Kathleen French <kfrench09@gmail.com>
2017-04-14 16:18:52 -0400

    wrote new rubocop tests for game controller

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,8 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-  describe 'game#new' do
-    it 'links to a landing page to create a game' do
+  describe 'game#index' do
+    it 'lists games with 1 white player but no black players in @games' do
+      game = create(:game_with_white_player)
+      get :index
+      expect(assigns(:games)).to include game
+    end
+
+    it 'lists games with 1 black player but no white players in @games' do
+      game = create(:game_with_black_player)
+      get :index
+      expect(assigns(:games)).to include game
+    end
+
+    it 'does not list games with 1 black player and 1 white player in @games' do
+      game = create(:game_with_white_and_black_players)
+      get :index
+      expect(assigns(:games)).not_to include game
+    end
+
+    it 'links to a modal form to successfully initiate a new game' do
       user = create(:user)
       sign_in user
       get :new
@@ -22,7 +40,19 @@ RSpec.describe GamesController, type: :controller do
       user = create(:user)
       sign_in user
       post :create, game: { name: 'test game' }
+      expect(assigns(:game).white_player_id).to eq(user.id)
       expect(response).to redirect_to game_path(assigns(:game))
+    end
+  end
+
+  describe 'game#join' do
+    it 'ensures that black player is joining free game and assigned id to match' do
+      user = create(:user)
+      sign_in user
+      game = create(:game_with_white_player)
+      patch :join, id: game
+      expect(assigns(:game).black_player_id).to eq(user.id)
+      expect(response).to redirect_to(game_path(game))
     end
   end
 
@@ -32,6 +62,20 @@ RSpec.describe GamesController, type: :controller do
     describe 'current game loads' do
       it 'returns the current_game w/o error' do
         get :show, id: game.id
+        expect(response).to be_success
+      end
+    end
+
+    context 'format.json' do
+      it 'succeeds' do
+        get :show, id: game.id, format: :json
+        expect(response).to be_success
+      end
+    end
+
+    context 'format.html' do
+      it 'succeeds' do
+        get :show, id: game.id, format: :html
         expect(response).to be_success
       end
     end
