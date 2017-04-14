20b82dad48731af1ccf6cc8a7e4dc940780ea870
Haley Mnatzaganian <haleymnatz@gmail.com>
2017-04-14 23:54:43 +0000

    Really deal with merge conflict

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,41 +5,40 @@ class Game < ApplicationRecord
 
   validates :name, presence: true
 
-<<<<<<< HEAD
+
   def find_piece(x_position, y_position)
     pieces.find_by(x_position: x_position, y_position: y_position)
   end
-=======
-    def make_newboard
-      # create and place white pieces
-      (0..7).each do |i|
-        Pawn.create(x_position: i, y_position: 1, color: 'White')
-      end
 
-      Rook.create(x_position: 0, y_position: 0, color: 'White')
-      Rook.create(x_position: 7, y_position: 0, color: 'White')
-      Knight.create(x_position: 1, y_position: 0, color: 'White')
-      Knight.create(x_position: 6, y_position: 0, color: 'White')
-      Bishop.create(x_position: 2, y_position: 0, color: 'White')
-      Bishop.create(x_position: 5, y_position: 0, color: 'White')
-      Queen.create(x_position: 3, y_position: 0, color: 'White')
-      King.create(x_position: 4, y_position: 0, color: 'White')
-
-      # create and place black pieces
-      (0..7).each do |i|
-        Pawn.create(x_position: i, y_position: 6, color: 'Black')
-      end
-
-      Rook.create(x_position: 0, y_position: 7, color: 'Black')
-      Rook.create(x_position: 7, y_position: 7, color: 'Black')
-      Knight.create(x_position: 1, y_position: 7, color: 'Black')
-      Knight.create(x_position: 6, y_position: 7, color: 'Black')
-      Bishop.create(x_position: 2, y_position: 7, color: 'Black')
-      Bishop.create(x_position: 5, y_position: 7, color: 'Black')
-      Queen.create(x_position: 3, y_position: 7, color: 'Black')
-      King.create(x_position: 4, y_position: 7, color: 'Black')
+  def make_newboard
+    # create and place white pieces
+    (0..7).each do |i|
+      Pawn.create(x_position: i, y_position: 1, color: 'White')
     end
->>>>>>> e7d9f7722dfc093819f2b97c7aa67ef3aca6450a
+
+    Rook.create(x_position: 0, y_position: 0, color: 'White')
+    Rook.create(x_position: 7, y_position: 0, color: 'White')
+    Knight.create(x_position: 1, y_position: 0, color: 'White')
+    Knight.create(x_position: 6, y_position: 0, color: 'White')
+    Bishop.create(x_position: 2, y_position: 0, color: 'White')
+    Bishop.create(x_position: 5, y_position: 0, color: 'White')
+    Queen.create(x_position: 3, y_position: 0, color: 'White')
+    King.create(x_position: 4, y_position: 0, color: 'White')
+
+    # create and place black pieces
+    (0..7).each do |i|
+      Pawn.create(x_position: i, y_position: 6, color: 'Black')
+    end
+
+    Rook.create(x_position: 0, y_position: 7, color: 'Black')
+    Rook.create(x_position: 7, y_position: 7, color: 'Black')
+    Knight.create(x_position: 1, y_position: 7, color: 'Black')
+    Knight.create(x_position: 6, y_position: 7, color: 'Black')
+    Bishop.create(x_position: 2, y_position: 7, color: 'Black')
+    Bishop.create(x_position: 5, y_position: 7, color: 'Black')
+    Queen.create(x_position: 3, y_position: 7, color: 'Black')
+    King.create(x_position: 4, y_position: 7, color: 'Black')
+  end
 
   def associate_pieces!(user, color)
     pieces.where(color: color).each do |piece|
