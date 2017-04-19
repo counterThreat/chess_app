b0b7024d33d0a8eb3ef7c2a09d542d406b7263c8
Kathleen French <kfrench09@gmail.com>
2017-04-19 15:50:51 -0400

    update

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,35 +13,35 @@ class Game < ApplicationRecord
   def make_newboard
     # create and place white pieces
     (0..7).each do |i|
-      Pawn.create(game_id: id, x_position: i, y_position: 1, color: 'White', user_id: white_player_id)
+      Pawn.create(game_id: id, x_position: i, y_position: 1, color: 'white', user_id: white_player_id)
     end
 
-    Rook.create(game_id: id, x_position: 0, y_position: 0, color: 'White', user_id: white_player_id)
-    Rook.create(game_id: id, x_position: 7, y_position: 0, color: 'White', user_id: white_player_id)
-    Knight.create(game_id: id, x_position: 1, y_position: 0, color: 'White', user_id: white_player_id)
-    Knight.create(game_id: id, x_position: 6, y_position: 0, color: 'White', user_id: white_player_id)
-    Bishop.create(game_id: id, x_position: 2, y_position: 0, color: 'White', user_id: white_player_id)
-    Bishop.create(game_id: id, x_position: 5, y_position: 0, color: 'White', user_id: white_player_id)
-    Queen.create(game_id: id, x_position: 3, y_position: 0, color: 'White', user_id: white_player_id)
-    King.create(game_id: id, x_position: 4, y_position: 0, color: 'White', user_id: white_player_id)
+    Rook.create(game_id: id, x_position: 0, y_position: 0, color: 'white', user_id: white_player_id)
+    Rook.create(game_id: id, x_position: 7, y_position: 0, color: 'white', user_id: white_player_id)
+    Knight.create(game_id: id, x_position: 1, y_position: 0, color: 'white', user_id: white_player_id)
+    Knight.create(game_id: id, x_position: 6, y_position: 0, color: 'white', user_id: white_player_id)
+    Bishop.create(game_id: id, x_position: 2, y_position: 0, color: 'white', user_id: white_player_id)
+    Bishop.create(game_id: id, x_position: 5, y_position: 0, color: 'white', user_id: white_player_id)
+    Queen.create(game_id: id, x_position: 3, y_position: 0, color: 'white', user_id: white_player_id)
+    King.create(game_id: id, x_position: 4, y_position: 0, color: 'white', user_id: white_player_id)
 
     # create and place black pieces
     (0..7).each do |i|
-      Pawn.create(game_id: id, x_position: i, y_position: 6, color: 'Black', user_id: white_player_id)
+      Pawn.create(game_id: id, x_position: i, y_position: 6, color: 'black', user_id: black_player_id)
     end
 
-    Rook.create(game_id: id, x_position: 0, y_position: 7, color: 'Black', user_id: white_player_id)
-    Rook.create(game_id: id, x_position: 7, y_position: 7, color: 'Black', user_id: white_player_id)
-    Knight.create(game_id: id, x_position: 1, y_position: 7, color: 'Black', user_id: white_player_id)
-    Knight.create(game_id: id, x_position: 6, y_position: 7, color: 'Black', user_id: white_player_id)
-    Bishop.create(game_id: id, x_position: 2, y_position: 7, color: 'Black', user_id: white_player_id)
-    Bishop.create(game_id: id, x_position: 5, y_position: 7, color: 'Black', user_id: white_player_id)
-    Queen.create(game_id: id, x_position: 3, y_position: 7, color: 'Black', user_id: white_player_id)
-    King.create(game_id: id, x_position: 4, y_position: 7, color: 'Black', user_id: white_player_id)
+    Rook.create(game_id: id, x_position: 0, y_position: 7, color: 'black', user_id: black_player_id)
+    Rook.create(game_id: id, x_position: 7, y_position: 7, color: 'black', user_id: black_player_id)
+    Knight.create(game_id: id, x_position: 1, y_position: 7, color: 'black', user_id: black_player_id)
+    Knight.create(game_id: id, x_position: 6, y_position: 7, color: 'black', user_id: black_player_id)
+    Bishop.create(game_id: id, x_position: 2, y_position: 7, color: 'black', user_id: black_player_id)
+    Bishop.create(game_id: id, x_position: 5, y_position: 7, color: 'black', user_id: black_player_id)
+    Queen.create(game_id: id, x_position: 3, y_position: 7, color: 'black', user_id: black_player_id)
+    King.create(game_id: id, x_position: 4, y_position: 7, color: 'black', user_id: black_player_id)
   end
 
   def associate_pieces!
-    pieces.where(color: 'Black').update(user_id: black_player_id)
+    pieces.where(color: 'black').update(user_id: black_player_id)
   end
 
   # scope method for determining which games do not have a black_player
