624036e1162855f366d747be4dfcc0d23af73727
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-04-19 20:44:20 +0000

    rubocop and rspec error fix

--- spec/factories/piece.rb
+++ spec/factories/piece.rb
@@ -31,12 +31,23 @@ FactoryGirl.define do
     moved false
     captured false
   end
-  
+
   factory :pawn do
     type "Pawn"
     color "white"
     x_position 1
     y_position 1
+    sequence(:game_id) { |a| a }
+    sequence(:user_id) { |b| b }
+    moved false
+    captured false
+  end
+
+  factory :king do
+    type "King"
+    color "white"
+    x_position 3
+    y_position 0
     sequence(:game_id) { |a| a }
     sequence(:user_id) { |b| b }
     moved false
