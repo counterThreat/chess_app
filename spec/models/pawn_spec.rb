b48e4595e41ba68ec29c1e9c126d91fb25047a8f
Kathleen French <kfrench09@gmail.com>
2017-04-19 14:38:36 -0400

    add tests to rspec for pawn

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,4 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
+  describe 'valid_move?' do
+    mygame = FactoryGirl.create(:game)
+    madonna = FactoryGirl.create(:user)
+    pawn = FactoryGirl.create(:pawn, game: mygame, user: madonna)
+
+    it 'returns true if valid_move? is true' do
+      newmove = pawn.valid_move?(1,2)
+      expect(newmove).to eq true
+    end
+
+    it 'returns false if valid_move? is false' do
+      newmove = pawn.valid_move?(4,2)
+    end
+  end
+
+  describe 'pawn_possible?' do
+    mygame = FactoryGirl.create(:game)
+    doug = FactoryGirl.create(:user)
+    pawn = FactoryGirl.create(:pawn, game: mygame, user: doug)
+
+    it 'returns true if valid_vertical_move? is correct' do
+      newmove = pawn.valid_vertical_move?(1, 2)
+      expect(newmove).to eq true
+    end
+
+    it 'returns true if valid_capture? is true' do
+      newmove = pawn.valid_capture?(2,2)
+      expect(newmove).to eq true
+    end
+
+    it 'returns false if y exceeds bounds' do
+      newmove = pawn.exceeds_bounds?(1, 4)
+      expect(newmove).to eq false
+    end
+  end
 end
