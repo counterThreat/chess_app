f49016e629e887b4e70615c42b739c9f73fcb2dd
DavidMichaelPena <alexamenos@gmail.com>
2017-04-20 14:19:35 +0000

    revising knight model

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,10 +1,7 @@
 class Knight < Piece
   def valid_move?(x_new, y_new)
-    if (x_new - x.position).abs == 2) && (y_new - y_position).abs == 1) ||
-      (x_new - x_position).abs == 1) && (y_new - y_position).abs == 2)
-      true
-    else
-      super
-    end
+    return false unless super(x_new, y_new)
+    return false unless (x_new - x.position).abs == 2 && (y_new - y_position).abs == 1 ||
+      (x_new - x_position).abs == 1 && (y_new - y_position).abs == 2
   end
-end
+
