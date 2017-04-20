84c4d9af9ca95307e08be144f1dc6ae5d3063cd1
DavidMichaelPena <alexamenos@gmail.com>
2017-04-20 17:08:02 +0000

    more rubo details

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,8 +1,8 @@
 class Knight < Piece
   def valid_move?(x_new, y_new)
-    if(x_new - x_position).abs == 2 && (y_new - y_position).abs == 1 ||
+    if (x_new - x_position).abs == 2 && (y_new - y_position).abs == 1 ||
       (x_new - x_position).abs == 1 && (y_new - y_position).abs == 2
-       true
+      true
     else 
       super
     end
