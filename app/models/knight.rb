2a1df14b99dba7df8f4e57b4ba4250733689c930
DavidMichaelPena <alexamenos@gmail.com>
2017-04-20 17:04:34 +0000

    fixing typo and super

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,8 +1,10 @@
 class Knight < Piece
   def valid_move?(x_new, y_new)
-    return false unless super(x_new, y_new)
-    return false unless
-    (x_new - x.position).abs == 2 && (y_new - y_position).abs == 1 ||
-    (x_new - x_position).abs == 1 && (y_new - y_position).abs == 2
+    if (x_new - x_position).abs == 2 && (y_new - y_position).abs == 1 ||
+       (x_new - x_position).abs == 1 && (y_new - y_position).abs == 2
+       true
+     else 
+      super
+    end
   end
 end
