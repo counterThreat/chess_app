0f628b6f7a5a2eb6381baea1acd66c2d247c3367
DavidMichaelPena <alexamenos@gmail.com>
2017-04-20 14:12:22 +0000

    revised knight model

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,12 +1,10 @@
 class Knight < Piece
-  # def valid_move?(x_position, y_position)
-  #   return false unless super(x, y)
-  #   return false if exposes_king_to_attack?(x, y)
-  #   move_range = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]]
-  #   available_moves = []
-  #   move_range.each do |dx, dy|
-  #     available_moves << [(x_position + dx), (y_position + dy)]
-  #   end
-  #   available_moves.include?([x_position, y_position])
-  # end
+  def valid_move?(x_new, y_new)
+    if (x_new - x.position).abs == 2) && (y_new - y_position).abs == 1) ||
+      (x_new - x_position).abs == 1) && (y_new - y_position).abs == 2)
+      true
+    else
+      super
+    end
+  end
 end
