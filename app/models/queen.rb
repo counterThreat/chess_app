a0034050f7b86f54867cc34f4b3fa6bbda6cb208
DavidMichaelPena <alexamenos@gmail.com>
2017-04-21 22:08:50 +0000

    refactoring queen-model

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -2,10 +2,10 @@ class Queen < Piece
   def valid_move?(x_new, y_new)
     if obstructed?(x_new, y_new)
       false
-    elsif diagonal_move?(x, y) || horizontal_move?(x, y) || vertical_move?(x, y)
+    elsif diagonal_move?(x_new, y_new) || horizontal_move?(x_new, y_new) || vertical_move?(x_new, y_new)
       true
     else
-      super
+      false
     end
   end
 end
