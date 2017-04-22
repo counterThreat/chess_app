e3c1108ba64ba02cffe2bc73dd599accc448393a
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-04-22 13:32:00 +0000

    fixed queen error causing move to not work

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -28,8 +28,7 @@ class Piece < ApplicationRecord
     if valid_move?(x_new, y_new) && on_board? && attack!(x_new, y_new) != false
       Piece.transaction do
         attack!(x_new, y_new)
-        update!(x_position: x_new)
-        update!(y_position: y_new)
+        update(x_position: x_new, y_position: y_new)
         if game.check == color
           raise ActiveRecord::Rollback, 'Move forbidden: exposes king to check'
         end
@@ -37,6 +36,9 @@ class Piece < ApplicationRecord
     else
       puts 'Move is not allowed!' # can change this to be a flash method
       return
+    end
+    if moved?
+      update(moved: true)
     end
   end
 
