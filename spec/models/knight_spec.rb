3d06d6893a84a3d4d3a1e150b9ee8f14136114d1
DavidMichaelPena <alexamenos@gmail.com>
2017-04-26 18:17:51 +0000

    fixing test coordinates

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -6,16 +6,20 @@ RSpec.describe Knight, type: :model do
   knight = FactoryGirl.create(:knight, game: mygame, user: rintrah)
 
   describe "valid_move? method" do
-    it "returns false for a forward jump" do
+    it "returns true for a forward jump" do
+      newmove = knight.valid_move?(2, 2)
+      expect(newmove).to eq true
+    end
+    it "returns true for a sideways move" do
+      newmove = knight.valid_move?(3, 1)
+      expect(newmove).to eq true
+    end
+    it "returns false for a illegal jump" do
       newmove = knight.valid_move?(3, 3)
       expect(newmove).to eq false
     end
-    it "returns false for a sideways move" do
-      newmove = knight.valid_move?(4, 2)
-      expect(newmove).to eq false
-    end
     it "returns true for a border move" do
-      newmove = knight.valid_move?(1, 3)
+      newmove = knight.valid_move?(0, 2)
       expect(newmove).to eq true
     end
   end
