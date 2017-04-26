19f0590097ec349aae544ef81a513888c0d72ff2
Kathleen French <kfrench09@gmail.com>
2017-04-26 18:47:51 -0400

    stopping point - y axis on tables goes top to bottom, not bottom to top, need fix

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,10 +1,11 @@
+
 function setBoard(){
   var url = window.location.href;
 
 // refreshes the board
   $.get(url + "/pieces").success(function(data){
-  for(var x = 0; x < 8; x++) {
-    for (var y = 0; y < 8; y++) {
+  for(var x = 0; x <= 7; x++) {
+    for (var y = 0; y <= 87; y++) {
       var square = $('#' + x + y);
       square.html('');
       console.log('refreshes board');
@@ -16,6 +17,7 @@ function setBoard(){
     data.forEach(function(piece){
       var cssSelector = "#" + piece.x_position + piece.y_position;
       var square = $(cssSelector);
+      console.log(square);
 
       var chess_piece = $('<div></div>');
       chess_piece.html(piece.unicode);
@@ -23,6 +25,9 @@ function setBoard(){
       chess_piece.attr('data-id', piece.id);
       chess_piece.attr('data-x-position', piece.x_position);
       chess_piece.attr('data-y-position', piece.y_position);
+      chess_piece.attr('piece-type', piece.type);
+      chess_piece.attr('piece-uniocde', piece.unicode);
+      chess_piece.attr('piece-user-id', piece.user_id);
 
       square.html('');
       square.html(chess_piece);
@@ -32,22 +37,29 @@ function setBoard(){
   });
 }
 
+function allowDrop(event){
+  event.preventDefault();
+}
+
 function handleDrag(element){
   // element that's being dropped
-  var square = $(this);
   var piece = $(element);
+  var square = $(this);
 
   var piece_id = piece.attr('data-id');
-  var destination_x = square.attr('data-x');
-  var destination_y = square.attr('data-y');
+  var dx = square.attr('data-x');
+  var dy = square.attr('data-y');
+  var user = piece.attr('piece-user-id');
+  var type = piece.attr('piece-type');
 
   var url = window.location.href + '/pieces/' + piece_id;
-  $.ajax({
+
+  $.ajax(url, {
     type: 'POST',
-    url: url,
-    data: { _method: 'PATCH', x_position: destination_x, y_position: destination_y },
-    success: function(msg){
-      console.log('success!');
+    data: { _method: 'PATCH', x_position: dx, y_position: dy },
+    //x_position: destination_x,
+    //y_position: destination_y,
+    success: function(data){
       setBoard();
     }
   });
@@ -56,7 +68,7 @@ function handleDrag(element){
 function dragDropPiece(){
   $('.piece').draggable({ containment: ".chessboard", snap: ".square"});
   $('.square').droppable({
-    onDrop: handleDrag
+    drop: handleDrag
   });
 }
 
