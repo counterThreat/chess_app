c242f1a9f1bd7c55bbbc7c7aa609eef32c8a67dc
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-04-26 20:27:41 +0000

    added column to pieces

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426165726) do
+ActiveRecord::Schema.define(version: 20170426202259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170426165726) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.string   "unicode"
+    t.integer  "move_num",   default: 0
   end
 
   create_table "users", force: :cascade do |t|
