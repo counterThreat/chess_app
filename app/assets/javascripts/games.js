419a50a57f1607b13777d37588030c366b59c95c
Kathleen French <kfrench09@gmail.com>
2017-04-28 16:07:12 -0400

    fixed x/y axis flip on /show

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -24,9 +24,6 @@ function setBoard(){
       chess_piece.attr('data-id', piece.id);
       chess_piece.attr('data-x-position', piece.x_position);
       chess_piece.attr('data-y-position', piece.y_position);
-      chess_piece.attr('piece-type', piece.type);
-      chess_piece.attr('piece-uniocde', piece.unicode);
-      chess_piece.attr('piece-user-id', piece.user_id);
 
       square.html('');
       square.html(chess_piece);
@@ -36,26 +33,22 @@ function setBoard(){
   });
 }
 
-function allowDrop(event){
-  event.preventDefault();
-}
-
-function handleDrag(element){
+function handleDrag(element, data){
   // element that's being dropped
-  var piece = $(element);
+  var chess_piece = $(element);
   var square = $(this);
 
-  var piece_id = piece.attr('data-id');
+  var piece_id = chess_piece.attr('data-id');
   var dx = square.attr('data-x');
   var dy = square.attr('data-y');
-  var user = piece.attr('piece-user-id');
-  var type = piece.attr('piece-type');
+  var user = chess_piece.attr('piece-user-id');
+  var type = chess_piece.attr('piece-type');
 
   var url = window.location.href + '/pieces/' + piece_id;
 
   $.ajax(url, {
-    type: 'POST',
-    data: { _method: 'PATCH', x_position: dx, y_position: dy },
+    type: 'PUT',
+    data: { piece: { x_position: dx, y_position: dy }, _method: 'patch' },
     //x_position: destination_x,
     //y_position: destination_y,
     success: function(data){
