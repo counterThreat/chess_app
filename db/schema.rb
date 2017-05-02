d6c9fda7d4e23d05c3309755637a7853742ca0b0
Haley Mnatzaganian <haleymnatz@gmail.com>
2017-05-02 18:30:24 +0000

    Add outcome and finished columns to games table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428221741) do
+ActiveRecord::Schema.define(version: 20170502182814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20170428221741) do
     t.datetime "updated_at",        null: false
     t.string   "name"
     t.integer  "winner_id"
+    t.string   "outcome"
+    t.datetime "finished"
     t.index ["winner_id"], name: "index_games_on_winner_id", using: :btree
   end
 
