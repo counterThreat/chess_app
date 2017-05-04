f1f06b45c057144aa1e19703a119e16ce5d4c37a
Anna Canelas <canelasa2@gmail.com>
2017-05-04 19:38:47 +0000

    Add turn logic

---
+++ db/migrate/20170502192345_add_turn_to_games_table.rb
@@ -0,0 +1,5 @@
+class AddTurnToGamesTable < ActiveRecord::Migration[5.0]
+  def change
+    add_column :games, :turn, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428221741) do
+ActiveRecord::Schema.define(version: 20170502192345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170428221741) do
     t.datetime "updated_at",        null: false
     t.string   "name"
     t.integer  "winner_id"
+    t.integer  "turn"
     t.index ["winner_id"], name: "index_games_on_winner_id", using: :btree
   end
 
