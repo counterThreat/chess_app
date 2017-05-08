b8aa3a73f35795a5b334ead5f6e6943b57ccbc3c
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-05-08 12:28:13 +0000

    rubocop fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,6 @@
 module UsersHelper
   def get_username(id)
     return User.find(id).username if User.exists?(id)
-    render html: "Player No Longer Active"
+    render html: 'Player No Longer Active'
   end
 end
