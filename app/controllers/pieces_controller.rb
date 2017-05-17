bce0df62098d5cc15dc4cb8fb784f2d14b8fb69b
Kathleen French <kfrench09@gmail.com>
2017-05-17 09:47:52 -0400

    PR ready

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -7,7 +7,7 @@ class PiecesController < ApplicationController
     user_id = params[:piece][:user_id]
     render plain: {'msg': 'Not your turn', 'piece_id': params[:piece][:id]}.to_json, status: :forbidden and return unless current_piece.game.send(:"#{current_piece.color}_player_id") == user_id.to_i && user_id.to_i == current_user.id
   end
-  
+
   def index
     @game = Game.find(params[:game_id])
     @pieces = Piece.where(game_id: @game.id)
@@ -28,7 +28,7 @@ class PiecesController < ApplicationController
     game = current_piece.game
     x = params[:piece][:x_position].to_i
     y = params[:piece][:y_position].to_i
-    puts "in update action"
+    puts 'in update action'
     current_piece.move(x, y)
     # && pusher_status
     Pusher.trigger("game-channel-#{game.id}", 'piece-moved', {
@@ -49,7 +49,7 @@ class PiecesController < ApplicationController
   def current_piece
     @piece ||= Piece.find(params[:id])
   end
-  
+
   private
 
   def url_status
