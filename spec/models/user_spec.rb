a42df558b3271ff7a953c3c03e936f8d1c6473de
Kathleen French <kfrench09@gmail.com>
2017-05-18 15:04:00 -0400

    update users controller spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe User, type: :model do
     it 'something' do
     end
   end
+  
+  describe '#user_data' do
+    it 'returns correct user data' do
+      user = create(:user)
+      username = user.username
+      gravatar_url = "https://secure.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email.downcase)}?s=75"
+      registration_date = user.created_at.strftime('%m-%d-%Y')
+      win_count = user.win_count
+      data = { name: username, gravatar_url: gravatar_url, member_since: registration_date, total_wins: win_count }
+      expect(user.user_data).to eq data
+    end
+  end
 end
