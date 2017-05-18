4ff8d36f045c919433ea9a4f6e05398a0af6f521
Kathleen French <kfrench09@gmail.com>
2017-05-18 14:33:33 -0400

    add button styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users.to_json(only: [:id, :username])
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
